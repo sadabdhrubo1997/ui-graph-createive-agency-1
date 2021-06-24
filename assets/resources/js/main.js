5c94089376b4daac865081033c55d4ba5d68fca4
sadabdhrubo1997 <53494264+sadabdhrubo1997@users.noreply.github.com>
2021-06-25 04:11:18 +0600

    testimonial slider done without responsive

---
+++ assets/resources/js/main.js
@@ -0,0 +1,11 @@
+$("#testimonialSliderWrapper").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+    autoplay: false,
+    autoplaySpeed: 5000,
+    dots: true,
+    draggable: false,
+    centerPadding: '33.3%',
+    centerMode: true,
+})
